72925c91f1694c90940e4a0a2d5366175dfdf60b
eak006 <erik.kaufman@live.com>
2019-03-17 18:07:03 -0400

    Created a bunch of overload constructors

--- src/net/SimpleHttpServer.java
+++ src/net/SimpleHttpServer.java
@@ -3,6 +3,7 @@ package net;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -14,6 +15,26 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class SimpleHttpServer {
 
     private HttpServer server;
+
+    public SimpleHttpServer() throws IOException {
+        this(null, new InetSocketAddress(8080), 0);
+    }
+
+    public SimpleHttpServer(int port) throws IOException {
+        this(null, new InetSocketAddress(port), 0);
+    }
+
+    public SimpleHttpServer(int port, int backlog) throws IOException {
+        this(null, new InetSocketAddress(port), backlog);
+    }
+
+    public SimpleHttpServer(Executor exec, int port) throws IOException {
+        this(exec, new InetSocketAddress(port), 0);
+    }
+
+    public SimpleHttpServer(Executor exec, int port, int backlog) throws IOException {
+        this(exec, new InetSocketAddress(port), backlog);
+    }
 
     public SimpleHttpServer(Executor exec, InetSocketAddress addr, int backlog) throws IOException {
         AtomicInteger counter = new AtomicInteger();
