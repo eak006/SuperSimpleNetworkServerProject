30b7760a6582baaf8340beb1ea8ee451d16117f4
eak006 <erik.kaufman@live.com>
2019-03-17 17:24:04 -0400

    Edits to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import net.SimpleHttpServer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Executor e = new Executor() {
+                @Override
+                public void execute(Runnable command) {
+                    System.out.println("Hey");
+                }
+            };
 
+            String hostname = "asdf";
+            int port = 8080;
+            InetSocketAddress isa = new InetSocketAddress(hostname, port);
+
+            int backlog = 0;
+
+            SimpleHttpServer shs = new SimpleHttpServer(e, isa, backlog);
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
