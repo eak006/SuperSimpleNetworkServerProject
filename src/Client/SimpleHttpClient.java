fc4a83c27f1c5f8a3b98d8d2874e9b93101b35b3
Nathan Darrah <nmd001@lvc.edu>
2019-03-25 09:10:59 -0400

    working on client

--- src/Client/SimpleHttpClient.java
+++ src/Client/SimpleHttpClient.java
@@ -1,11 +1,18 @@
 package Client;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SimpleHttpClient {
 
     private URL url;
     HttpURLConnection con;
+
+    public SimpleHttpClient() throws IOException {
+        url = new URL("http://0.0.0.0:8081/get");
+        con = (HttpURLConnection) url.openConnection();
+    }
 
 }
